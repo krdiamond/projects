82cf2311b3eab22ec465af3a6098326fa2d5bfe2
Kristina Diamond <krdiamond@gmail.com>
2023-05-29 21:11:07 -0400

    Revert "updated icons to not overlap"
    This reverts commit e2afa1549a949ab14d538257589cc961b6840108.

--- src/components/RandomDots.vue
+++ src/components/RandomDots.vue
@@ -3,10 +3,7 @@
     <div class="height--full">
       <div ref="main" class="position--center position--absolute">
         <div ref="box" class="box position--absolute ag-icon_item" v-for="(icon, index) in iconsWithStats" :key="'icon-' + index" :style="icon.box.style" :class="{ 'transition': isTransition }">
-          <img v-if="icon.copyText" @click="copyToClipboard(icon.copyText)" :src="icon.svg" alt="" />
-          <a v-else :href="icon.link" target="_blank" :aria-label="icon.ariaLabel">
-            <img :src="icon.svg" alt="" />
-          </a>
+          <img :src="icon.svg" :alt="icon.link" class="icon-image" />
         </div>
       </div>
     </div>
@@ -14,11 +11,6 @@
 </template>
 
 <script>
-import linkedIn from '../assets/linkedin.svg';
-import gitHub from '../assets/github.svg';
-import mail from '../assets/mail.svg';
-import resume from '../assets/resume.svg';
-
 export default {
   name: 'RandomDots',
   data() {
@@ -27,33 +19,20 @@ export default {
       isTransition: false, // Indicates if the transition animation should be applied
       icons: [
         {
-          svg: linkedIn,
-          link: 'https://www.linkedin.com/in/kristinadiamond/',
-          ariaLabel: "Navigate to Kristina Diamond's Linkedin"
+          svg: 'OSSA New York',
+          link: 'https://www.ossanewyork.com/',
         },
         {
-          svg: gitHub,
-          link: 'https://github.com/krdiamond',
-          ariaLabel: "View Kristina Diamond's Github"
-        },
-        {
-          copyText: 'krdiamond@gmail.com',
-          svg: mail,
-          link: 'https://github.com/krdiamond',
-          ariaLabel: "View Kristina Diamond's Github"
-        },
-        {
-          svg: resume,
-          link: 'KristinaDiamondResume.pdf',
-          ariaLabel: "View Kristina Diamond's Resume"
+          svg: 'OSSA New York',
+          link: 'https://www.ossanewyork.com/',
         },
       ],
     };
   },
   mounted() {
     this.updateWindowSize();
-    window.addEventListener('resize', this.handleResize);
     this.initFloating();
+    window.addEventListener('resize', this.handleResize);
   },
   computed: {
     iconsWithStats() {
@@ -175,19 +154,6 @@ export default {
         item.style.transition = '5s';
       }, 3500);
     },
-    copyToClipboard(text) {
-      navigator.clipboard.writeText(text)
-        .then(() => {
-          alert("Kristina's e-mail address is copied to your clipboard. krdiamond@gmail.com");
-        })
-        .catch((error) => {
-          console.error('Failed to copy text: ', error);
-        });
-    }
   },
 };
 </script>
-
-<style>
-/* Add your custom styles here */
-</style>
