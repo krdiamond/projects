0e4ac440847e1fa9cbfe39c7de6bd714e68b2038
Kristina Diamond <krdiamond@gmail.com>
2023-05-31 19:06:48 -0400

    fixing window height

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div ref="main" class="home container margin--20 flex flex--col justify--center">
+  <div ref="main" class="home container margin--20 flex flex--col " :class="{ 'justify--center': !horizontalLayout }"> 
       <header>
-        <h1 v-if="horizontalLayout" class="hor">KRISTINA DIAMOND</h1>
-        <h1 v-else class="vert flex flex--col block--md">
+        <!-- <h1 v-if="horizontalLayout" class="hor">KRISTINA DIAMOND</h1> -->
+        <h1 class="vert flex flex--col block--md">
               <span class="padding-right--20--md">KRISTINA</span>
               <span class="space_x1 no-letter-spacing--md">DIAMOND</span>
           </h1>
@@ -46,15 +46,26 @@ export default {
       this.updateWindowSize();
       window.addEventListener("resize", this.updateWindowSize);
   },
+  computed: {
+    isPhoneHorizontal() {
+      return (window.orientation === 90 || window.orientation === -90);
+    }
+  },
   methods: {
     updateWindowSize() {
       const mainWidth = this.$refs.main.clientWidth;
       const mainHeight = this.$refs.main.clientHeight;
-      if ((mainWidth > mainHeight) ||  (window.orientation === 90 || window.orientation === -90)) {
+      if ((mainWidth < 768) & (mainHeight < 400)) {
         this.horizontalLayout = true;
-      } else {
+      } else if ((mainWidth > 768) & (mainWidth < 1440) & (mainHeight < 730)) {
+        this.horizontalLayout = true;
+      } else if ((mainWidth > 1440) & (mainHeight < 950)) {
+        this.horizontalLayout = true;
+      } 
+      else {
         this.horizontalLayout = false;
       }
+
     },
   },
 }
