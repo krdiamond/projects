825865602d7d9b04cd1531dc04e840c47e6cab4d
Kristina Diamond <krdiamond@gmail.com>
2023-06-17 23:16:11 -0400

    updating module exports

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "http://www.kristinadiamond.com/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 };
