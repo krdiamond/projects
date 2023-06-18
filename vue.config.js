50c7f9ebeb79fe9b67f4d3f63422e72f7541c086
Kristina Diamond <krdiamond@gmail.com>
2023-06-18 09:21:22 -0400

    dependencies

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,11 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 };
+
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+//     .BundleAnalyzerPlugin;
+//     module.exports = {
+//         configureWebpack: {
+//             plugins: [new BundleAnalyzerPlugin()]
+//         }
+//    };
