1f711d55e98c9aeabb48a4da20b8e3c47eb95418
Kristina Diamond <krdiamond@gmail.com>
2023-06-18 22:31:06 -0400

    removed stroke and made the bubbles solid

--- src/components/ConfettiContainer.vue
+++ src/components/ConfettiContainer.vue
@@ -1,64 +1,58 @@
 <template>
-      <canvas id="spawncontainer"></canvas>
-  </template>
-  
-  <script>
-  
-  
-  export default {
-    name: 'ConfettiContainer',
-    data() {return {}},
-    mounted() {
-        this.confetti();
-    },
-    methods: {
-        confetti() {
-      var colors = [ '#4200FF' ];
-      var shapes = ['arc'];
-      var bubbles = 15;
-      var spawnc = document.getElementById("spawncontainer");
-      var spawnctx = spawnc.getContext("2d");
+  <canvas ref="spawncontainer"></canvas>
+</template>
+
+<script>
+export default {
+  name: 'ConfettiContainer',
+  mounted() {
+    this.confetti();
+  },
+  methods: {
+    confetti() {
+      const colors = ['#4200FF'];
+      const shapes = ['arc'];
+      const bubbles = 15;
+      const spawnc = this.$refs.spawncontainer;
+      const spawnctx = spawnc.getContext('2d');
 
       function render(particles, spawnctx, width, height) {
         requestAnimationFrame(() => render(particles, spawnctx, width, height));
         spawnctx.clearRect(0, 0, width, height);
         particles.forEach((p) => {
-          p.x += p.speed * Math.cos(p.rotation * Math.PI / 180);
-          p.y += p.speed * Math.sin(p.rotation * Math.PI / 180);
+          p.x += p.speed * Math.cos((p.rotation * Math.PI) / 180);
+          p.y += p.speed * Math.sin((p.rotation * Math.PI) / 180);
           p.opacity -= 0.01;
           p.speed *= p.friction;
           p.radius *= p.friction;
           p.yVel += p.gravity;
           p.y += p.yVel;
-          if(p.opacity < 0 || p.radius < 0) return;
+          if (p.opacity < 0 || p.radius < 0) return;
           spawnctx.beginPath();
           spawnctx.globalAlpha = p.opacity;
-          spawnctx.strokeStyle = p.color;
-          if(p.shape == "arc"){
+          spawnctx.fillStyle = p.color;
+          if (p.shape == 'arc') {
             spawnctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, false);
-          } 
-          spawnctx.lineWidth = p.radius;
-          spawnctx.stroke();
+          }
+          spawnctx.fill();
         });
-        return spawnctx;
       }
 
-      var r = (a, b, c) => parseFloat((Math.random() * ((a ? a : 1) - (b ? b : 0)) + (b ? b : 0)).toFixed(c ? c : 0));
+      const r = (a, b, c) => parseFloat((Math.random() * ((a ? a : 1) - (b ? b : 0)) + (b ? b : 0)).toFixed(c ? c : 0));
 
-      function spawn(event){
-        var mousex = event.clientX;
-        var mousey = event.clientY;
-        var particles = [];
-        var setRadius = 0;
-        
+      function spawn(event) {
+        const mousex = event.clientX;
+        const mousey = event.clientY;
+        const particles = [];
+        let setRadius = 0;
+
         if (window.innerWidth < 768) {
-            setRadius = 10;
+          setRadius = 10;
+        } else {
+          setRadius = 20;
         }
-        else {
-            setRadius = 20;
-        }
-        
-        for(var i = 0; i < bubbles; i++) {
+
+        for (let i = 0; i < bubbles; i++) {
           particles.push({
             x: mousex,
             y: mousey,
@@ -70,20 +64,15 @@
             friction: 0.9,
             opacity: r(0, 0.5, true),
             yVel: 0,
-            gravity: 0.1
+            gravity: 0.1,
           });
         }
-        
+
         render(particles, spawnctx, spawnc.width, spawnc.height);
       }
 
-      document.onclick = spawn; 
-    }
-      
+      document.onclick = spawn;
     },
-  }
-  
-  </script>
-  
-  
-  <!-- alt="Vue logo" src="./assets/logo.png">-->
+  },
+};
+</script>
