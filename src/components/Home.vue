53d5fed012f32cf80a1ba9e673944e1613f609a8
Kristina Diamond <krdiamond@gmail.com>
2026-06-20 13:00:37 -0400

    fixed mobile sizing

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,34 @@
 <template>
-  <div class="container flex flex--col align--center justify--center overflow--unset">
-    <div ref="main" class="home container flex flex--col align--center justify--center overflow--unset"> 
+  <div class="home-shell container flex flex--col overflow--hidden height--full width--full">
+    <div
+      ref="main"
+      class="home home-shell__content container flex flex--col align--center justify--center flex--1 overflow--hidden"
+    >
       <header class="position--relative">
         <Eyes :isTouchDevice="isTouchDevice" class="eyes position--absolute"/>
         <h1 class="flex flex--col position--relative">
-            <span >KRISTINA</span>
-            <span class="space_x1">DIAMOND</span>
+          <span>KRISTINA</span>
+          <span class="space_x1">DIAMOND</span>
         </h1>
         <p>front end web developer <br> specializing in shopify</p>
       </header>
       <section>
         <button class="blob-btn" @click="$emit('open-portfolio')">
-            PROJECTS
-            <span class="blob-btn__inner">
-                <span class="blob-btn__blobs">
-                    <span class="blob-btn__blob"></span>
-                    <span class="blob-btn__blob"></span>
-                    <span class="blob-btn__blob"></span>
-                    <span class="blob-btn__blob"></span>
-                </span>
+          PROJECTS
+          <span class="blob-btn__inner">
+            <span class="blob-btn__blobs">
+              <span class="blob-btn__blob"></span>
+              <span class="blob-btn__blob"></span>
+              <span class="blob-btn__blob"></span>
+              <span class="blob-btn__blob"></span>
             </span>
+          </span>
         </button>
         <!-- <BouncingIcons id="BouncingIcons" @click="$emit('open-email')"/> -->
       </section>
-
+    </div>
+    <Marquee class="home-shell__marquee flex--shrink-0"/>
   </div>
-      <Marquee/>
-  </div>
-  
 </template>
 
 <script>
